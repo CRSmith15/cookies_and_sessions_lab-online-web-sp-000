0c2423811052d895f0c64c5c951e15c0d92e7fbc
Chance Smith <chancesmith97@hotmail.com>
2020-08-28 22:26:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'products#index', as: :products
+
+  post '/add', to: 'products#add', as: :add_product
 end
